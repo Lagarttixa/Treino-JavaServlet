2bc853045585d74e8ed00047805cddbd75850f5e
Abner Machado <abnermachado@sisconsultoria.com.br>
2022-03-09 16:32:55 -0300

    quarta alteração

--- src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,6 +1,11 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,7 +13,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+/**
+ * Servlet implementation class NovaEmpresaServlet
+ */
 @WebServlet("/novaEmpresa")
 public class NovaEmpresaServlet extends HttpServlet {
 	
@@ -19,17 +26,31 @@ public class NovaEmpresaServlet extends HttpServlet {
 		System.out.println("Cadastrando nova empresa");
 		
 		String nomeEmpresa = request.getParameter("nome");
+		String paramDataEmpresa = request.getParameter("data");
+		
+		Date dataAbertura = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			dataAbertura = sdf.parse(paramDataEmpresa);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
+		
 		Empresa empresa = new Empresa();
 		empresa.setNome(nomeEmpresa);
+		empresa.setDataAbertura(dataAbertura);
 		
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
-		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada;jsp");
 		request.setAttribute("empresa", empresa.getNome());
-		rd.forward(request,response);
 		
+		response.sendRedirect("listaEmpresas");
 		
+//		//chamar o JSP ou Servlet
+//		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas");
+//		request.setAttribute("empresa", empresa.getNome());
+//		rd.forward(request, response);
 	}
 
 }
